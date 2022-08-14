924e584a387f4ca78ae2153ac0764654d529b843
skizzzy <86366610+awangui@users.noreply.github.com>
2022-08-14 21:14:03 +0300

    new task functionality

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-document.querySelector("i.edit");
+
+var addTask = document.getElementById("add");
+var incompleteTasks = document.getElementById("incomplete");
+var completeTasks = document.getElementById("complete");
+var i=0;
+
+document.querySelector("button").addEventListener("click", createNewTask);
+
+function createNewTask(){
+	var newTask = document.getElementById("new-task").value;
+	var listItem = document.createElement("li");
+	listItem.innerHTML= newTask;
+	console.log(listItem);
+	incompleteTasks.appendChild(listItem);
+	i++;
+
+}
+document.getElementById("delete").addEventListener("click",deleteTask);
+function deleteTask(){
+	
+
+}
