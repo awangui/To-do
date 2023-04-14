89b7dfa634d633885c8cdec366280d40b0b79124
wangui <86366610+awangui@users.noreply.github.com>
2023-04-14 11:42:38 +0300

    new task functionality

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
-
-var addTask = document.getElementById("add");
-var incompleteTasks = document.getElementById("incomplete");
-var completeTasks = document.getElementById("complete");
-var i=0;
-
-document.querySelector("button").addEventListener("click", createNewTask);
-
-function createNewTask(){
-	var newTask = document.getElementById("new-task").value;
-	var listItem = document.createElement("li");
-	listItem.innerHTML= newTask;
-	console.log(listItem);
-	incompleteTasks.appendChild(listItem);
-	i++;
-
+function newTask(){
+	var li = document.createElement('li');
+	var input = document.getElementById("new-task").value;
+	var task = document.createTextNode(input);
+	li.append(task);
+	document.getElementById("incomplete").appendChild(li);
+	//clear the input field
+	document.getElementById("new-task").value="";
 }
-document.getElementById("delete").addEventListener("click",deleteTask);
-function deleteTask(){
-	
-
+var list = document.querySelector('ul');
+list.addEventListener('click', function(check){
+	if(check.target.tagName === 'LI'){
+		check.target.classList.toggle('checked');
+	}
+}, false);
+//adding a close icon to each item
+var taskList = document.getElementsByTagName("LI");
+var i;
+for(i=0; i<taskList.length; i++){
+	var span = document.createElement("SPAN");
+	var txt=document.createTextNode("\u00D7");
+	span.className = "delete";
+	span.appendChild(txt);
+	taskList[i].appendChild(span);
+}
+//giving close icon functionality
+var close = document.getElementsByClassName("delete");
+var i;
+for(i=0; i<close.length; i++){
+	close[i].onclick = function(){
+		var div=this.parentElement;
+		div.style.display="none";
+	}
 }
